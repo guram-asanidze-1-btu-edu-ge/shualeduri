bfe21efc9530c52ab4fc9d91ca54fa7ee12f68fb
guram-asanidze-1-btu-edu-ge <131129552+guram-asanidze-1-btu-edu-ge@users.noreply.github.com>
2023-07-01 23:03:04 +0400

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -12,3 +12,8 @@ class QuadraticEquationSolver:
 
         if d == 0:
             return [-self.b / (2 * self.a)]
+
+        return [
+            (-self.b + d ** 0.5) / (2 * self.a),
+            (-self.b - d ** 0.5) / (2 * self.a),
+        ]
